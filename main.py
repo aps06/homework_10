f9e0b098e50d1afd5b6b0716eb160275a75b4619
Taras <93609222+aps06@users.noreply.github.com>
2023-12-24 17:17:04 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,68 @@
+from collections import UserDict
+
+
+class Field:
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
+
+
+class Name(Field):
+    def __init__(self, name):
+        self.value = name
+
+
+class Phone(Field):
+
+    def __init__(self, phone):
+        corect = False
+        for i in phone:
+            if i not in '0123456789':
+                corect = True
+        if corect or len(phone) != 10:
+            raise ValueError
+        self.value = phone
+
+
+class Record:
+    def __init__(self, name):
+        self.name = Name(name)
+        self.phones = []
+
+    def add_phone(self, phone):
+        self.phones.append(Phone(phone))
+
+    def remove_phone(self, phone):
+        for p in self.phones:
+            if phone == p.value:
+                self.phones.remove(p)
+
+    def edit_phone(self, phone_1, phone_2):
+        for i in self.phones:
+            if i.value == phone_1:
+                i.value = phone_2
+                return
+        raise ValueError
+
+    def find_phone(self, phone):
+        for p in self.phones:
+            if phone == p.value:
+                return p
+
+    def __str__(self):
+        return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
+
+
+class AddressBook(UserDict):
+    def add_record(self, value):
+        self.data.update({value.name.value: value})
+
+    def find(self, value):
+        if value in self.data:
+            return self.data[value]
+
+    def delete(self, value):
+        if value in self.data:
+            del self.data[value]
